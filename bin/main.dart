48d7fb02c0a9ab73f7f40715f7b4073038b2d29b
Dennis Bozzi <dennisbozzii@gmail.com>
2023-07-04 20:25:06 -0300

    Criando função recursiva, ela funciona muito parecida com um for. Opcional para criação de loops.

--- bin/main.dart
+++ bin/main.dart
@@ -3,12 +3,38 @@ void main() {
   double peso = 100.2;
   String corFruta = 'Verde e Amarelo';
   String sabor = 'Doce e cítrica';
-  int diasDeColheita = 40;
+  int diasDeColheita = 30;
   bool isMadura = funcIsMadura(diasDeColheita);
 
   print('A fruta está madura? $isMadura');
 
   funcShowMadura(nome, diasDeColheita, cor: corFruta, peso: peso);
+
+  funcQuantosDiasMadura(diasDeColheita);
+
+  funcRecursiva(0, 4);
+}
+
+//Usada como loop
+funcRecursiva(int contador, int limite) {
+  print('$contador');
+
+  if (contador >= limite) {
+    return;
+  }
+
+  funcRecursiva(contador + 1, limite);
+}
+
+funcQuantosDiasMadura(int dias) {
+  int diasParaMadura = 30;
+  int quantosDiasFalta = 30 - dias;
+
+  if (quantosDiasFalta <= 0) {
+    print('A fruta está madura!');
+  } else {
+    print('Faltam $quantosDiasFalta para a fruta amadurecer');
+  }
 }
 
 // 1 - Posicionais Obrigatórios - Posicionamento dos parametros na ordem correta (String nome, int dias)
@@ -16,14 +42,15 @@ void main() {
 // 3 - Parâmetros "Padrão" - Passa um valor default, para um parametro {String cor = "Sem cor"}
 // 4 - Modificador "Required" - Ele é um parametro nomeado que é obrigatório {required double peso}
 
-void funcShowMadura(String nome, int dias, {String? cor, required double peso}) {
+void funcShowMadura(String nome, int dias,
+    {String? cor, required double peso}) {
   if (dias >= 30) {
     print('A $nome está madura');
   } else {
     print('A $nome está verde');
   }
 
-  if(cor != null){
+  if (cor != null) {
     print('A cor da $nome é $cor');
   }
 
