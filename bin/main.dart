de38e30b2fac733cf093a752dcc41b285bef5c82
Dennis Bozzi <dennisbozzii@gmail.com>
2023-07-05 10:06:12 -0300

    Aporendendo o que são heranças.

--- bin/main.dart
+++ bin/main.dart
@@ -1,20 +1,89 @@
 void main() {
-  String nome = 'Laranja';
-  double peso = 100.2;
-  String corFruta = 'Verde e Amarelo';
-  String sabor = 'Doce e cítrica';
-  int diasDeColheita = 30;
-  bool isMadura;
-
   Fruta fruta01 = Fruta('Laranja', 100.2, 'Verde e Amarelo', 'Doce e cítrica', 30);
   Fruta fruta02 = Fruta('Uva', 80.3, 'Roxo', 'Doce', 15);
 
-  //print('A primeira fruta é a ${fruta01.nome} e a segunda fruta é a ${fruta02.nome}');
+  Legumes legumes01 = Legumes("Cenoura", 25.5, "Laranja", true);
+  Citricas citrica01 = Citricas('Pitanga', 0.2, 'Vermelha', 'Azeda', 35, 0.2);
+  Nozes nozes01 = Nozes("Noz", 0.5, "Marrom", "Gostoso", 80, 0.04);
 
   fruta01.estaMadura(50);
   fruta02.estaMadura(20);
+
+  legumes01.printAlimento();
+  legumes01.cozinhar();
+
+  citrica01.citrica();
+  nozes01.qtdOleoNatural();
 }
 
+class Alimento {
+  String nome;
+  double peso;
+  String cor;
+
+  Alimento(this.nome, this.peso, this.cor);
+
+  void printAlimento() {
+    print('Este(a) $nome pesa $peso gramas e é $cor');
+  }
+}
+
+class Fruta extends Alimento {
+  String sabor;
+  int diasDeColheita;
+  bool? isMadura;
+
+  Fruta(nome, peso, cor, this.sabor, this.diasDeColheita, {this.isMadura}) : super(nome, peso, cor);
+
+  void estaMadura(int diasParaMadura) {
+    isMadura = diasDeColheita >= diasParaMadura;
+    print('A sua $nome foi colhida a $diasDeColheita dias, e precisa de $diasParaMadura dias para amadurecer. Ela está madura? $isMadura');
+  }
+
+  void fazerSuco() {
+    print('Você acabou de fazer um suco de $nome');
+  }
+}
+
+class Legumes extends Alimento {
+  bool isPrecisaCozinhar;
+
+  Legumes(nome, peso, cor, this.isPrecisaCozinhar) : super(nome, peso, cor);
+
+  void cozinhar() {
+    if (isPrecisaCozinhar) {
+      print('O(a) $nome está cozinhando.');
+    } else {
+      print('Nem precisa cozinhar');
+    }
+  }
+}
+
+class Citricas extends Fruta {
+  double nivelCitrica;
+
+  Citricas(nome, peso, cor, sabor, diasDeColheita, this.nivelCitrica) : super(nome, peso, cor, sabor, diasDeColheita);
+
+  void citrica() {
+    if (nivelCitrica > 2) {
+      print('Esta fruta é muito cítrica - $nome');
+    } else {
+      print('Está fruta não é tão cítrica - $nome');
+    }
+  }
+}
+
+class Nozes extends Fruta {
+  double porcentagemDeOleoNatural;
+
+  Nozes(nome, peso, cor, sabor, diasDeColheita, this.porcentagemDeOleoNatural) : super(nome, peso, cor, sabor, diasDeColheita);
+
+  void qtdOleoNatural() {
+    print('O(a) $nome possui ${porcentagemDeOleoNatural * 100} por cento de óleo natural');
+  }
+}
+
+/*
 // 1 - Posicionais Obrigatórios - Posicionamento dos parametros na ordem correta (String nome, int dias)
 // 2 - Nomeados Opcionais - Passa um parametro nomeado que não é obrigatório {String? cor}
 // 3 - Parâmetros "Padrão" - Passa um valor default, para um parametro {String cor = "Sem cor"}
@@ -53,7 +122,6 @@ void funcShowMadura(String nome, int dias, {String? cor, required double peso}) 
   print('O peso da $nome é $peso');
 }
 
-//Usada como loop
 funcRecursiva(int contador, int limite) {
   print('$contador');
 
@@ -62,20 +130,4 @@ funcRecursiva(int contador, int limite) {
   }
 
   funcRecursiva(contador + 1, limite);
-}
-
-class Fruta {
-  String nome;
-  double peso;
-  String corFruta;
-  String sabor;
-  int diasDeColheita;
-  bool? isMadura;
-
-  Fruta(this.nome, this.peso, this.corFruta, this.sabor, this.diasDeColheita, {this.isMadura});
-
-  void estaMadura(int diasParaMadura) {
-    isMadura = diasDeColheita >= diasParaMadura;
-    print('A sua $nome foi colhida a $diasDeColheita dias, e precisa de $diasParaMadura dias para amadurecer. Ela está madura? $isMadura');
-  }
-}
+} */
